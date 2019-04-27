36ff1df61ba8e8d948c990b72c34065770b61262
Mohsen <mohsensajjaditabar@gmail.com>
2019-04-27 08:46:42 +0430

    V1comment

--- push to talk conference sample/src/test/java/att/com/ConferenceSample/ConferenceSampleMachineTest.java
+++ push to talk conference sample/src/test/java/att/com/ConferenceSample/ConferenceSampleMachineTest.java
@@ -30,6 +30,9 @@ import static org.hamcrest.Matchers.*;
 import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.gargoylesoftware.htmlunit.WebClient;
 
+
+
+// این کلاس از کلاسهای خود E4ss ارث بری کرده و قابلیتهای اون رو هم اکستند کرده.
 public final class ConferenceSampleMachineTest extends org.echarts.test.sip.CATTestCase {
     /** Version control identifier strings. */
     public static final String[] RCS_ID = {
@@ -45,19 +48,20 @@ public final class ConferenceSampleMachineTest extends org.echarts.test.sip.CATT
 	String outputDir;
 	Logger logger;
 	int remoteListenPort;
-	
+
+	// کانستراکتور کلاس.
 	public ConferenceSampleMachineTest() throws Exception {
-		logger = this.getLogger();
-		Properties props = new Properties();
+		logger = this.getLogger(); //این تابع getLogger برای خود E4SS هستش.
+		Properties props = new Properties(); // java.util.properties برای خود جاوا هستش.
 		try {
 			// Using getResourceAsStream(...) will read the properties file
 			// copied from src/test/resources
 			//
 			// props.load(new FileInputStream("test.properties"));
 			//
-			File propertiesFile = new File("test.properties");
+			File propertiesFile = new File("test.properties"); //تو test/resources تعریف شده
 			if (propertiesFile.exists()) {
-				props.load(new FileInputStream(propertiesFile));
+				props.load(new FileInputStream(propertiesFile)); // فایل properties رو لود میکنه.
 			}
 			else {
 				props.load(
@@ -70,13 +74,15 @@ public final class ConferenceSampleMachineTest extends org.echarts.test.sip.CATT
 			// file.
 			props.putAll(System.getProperties());
 
+			// اینها همه تو test.properties باید ست بشن:
 			appServer           = props.getProperty("SipAS");
 			httpServer          = props.getProperty("HttpAS");
 			sipListenPort       = Integer.parseInt(props.getProperty("SipStackListenPort"));
+			// اولین آرگومان ورودی میگه اگه این تو فایل بود مقدارش رو بده و دومی میگه اگه نبود این مقدار دیفالته
 			sipListenAddress    = props.getProperty("SipStackListenIP", InetAddress.getLocalHost().getHostAddress());
 			outputDir           = props.getProperty("OutputDir", "out");
 			appName             = props.getProperty("AppName", "ConferenceSampleTest");
-
+			// تابع داخلی همین کلاسه.
 			init("ConferenceSampleTest");
 		}
 		catch (Exception e) {
@@ -86,22 +92,33 @@ public final class ConferenceSampleMachineTest extends org.echarts.test.sip.CATT
 	}
 
 	private void init(String testName) throws CATException {
+		//این CATCOonfig تو خود E4ss و پکیج org.echarts.test.sipتعریف شده.
 		CATConfig config = new CATConfig();
 		config.setListenIP(this.sipListenAddress);
 		config.setListenPort(this.sipListenPort);
 		config.setTestName(testName);
 		config.setOutputDir(outputDir);
+
+		// این init تابع کلاس پدره(واسه E4SS) که CATConfig ورودی میگیره و کانفیگ رو ست میکنه.
 		this.init(config);
 	}
 
-	@BeforeClass static public void runOnceBeforeAllTests() {		
-	}
 
+	// ‌این @BeforeClass , @Before, @test همه تو org.junit هستن.
+	// Before -> test -> After
+
+	// این علامت @BeforeClass نشون میده که این تابع runOnceBofreAllTests همیشه باید
+	// قبل از اینکه یه نمونه از کلاس ساخته بشه،‌اجرا بشه.
+	@BeforeClass static public void runOnceBeforeAllTests() {
+	}
+	// این هم مثل بالایی فقط بعد از اینکه کلاس نال شد و گاربج کالکتور، صدا زده شد.
 	@AfterClass static public void runOnceAfterAllTests() {
 	}
 
+	// فقط تو لاگ مینیوسه که داریم تست میگیریم.
 	@Before public void runBeforeEachTest() {
 		try {
+			// دستور logger.info تو لاگ فایل مینویسه.
 			logger.info("running test setup");
 		}
 		catch (Exception e) {
@@ -109,6 +126,7 @@ public final class ConferenceSampleMachineTest extends org.echarts.test.sip.CATT
 		}
 	}
 
+	// میگه کار تموم شد و بعد منابع رو آزاد میکنه.
 	@After public void runAfterEachTest() {
 		try {
 			logger.info("running test cleanup");
@@ -120,6 +138,8 @@ public final class ConferenceSampleMachineTest extends org.echarts.test.sip.CATT
 		logger.info("====================");
 	}
 
+	// اینم تابع تست که قراره یه تماس رو به صورت کامل تست کنه. البته پیام صوتی منتقل نمیشه.
+	// و به محض اینکه طرف جواب بده، تلفن قطع میشه. یعنی فقط SIP فقط بررسی شده و RTP و  هنوز تست نشده.
 	/** Test of a completed call from caller to callee.  
 	 *  Caller ends call after callee answers.  RTP is
 	 *  not tested.
@@ -128,10 +148,11 @@ public final class ConferenceSampleMachineTest extends org.echarts.test.sip.CATT
 	 */
 	@Test public void testCompletedCall() throws Throwable {
 		try {
+			// مقدار دهی اولیه پارامترها!‌ البته نیازی نیست چون تو کانستراکتور انجام شده بود.
 			this.init("testCompletedCall");
 
 			
-			
+			//  دو SIPAgent به اسمهای alice و bob درست میکنیم. تابع createAgent تو کلاس پدر هستش.
 			SIPAgent alice = createAgent("alice");
 			SIPAgent bob = createAgent("bob");
 			
